42fdeb080572de9b8050c3272c504179cb6fda79
Finn <derfinn@gmail.com>
2014-03-11 00:33:15 +0100

    Add Bootstrap for feeling better

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ end
 get '/task', :provides => [:html, :json] do
   #return good or bad task
   good = ["duolingo", "fuck", "kiss", "play", "Portal"]
-  bad = ["clean something", "do sports", "eat apple", "do Uni", "call mum"]
+  bad = ["clean something", "do sports", "eat apple", "do Uni", "call mum", "go to bed"]
 
   good_probability = 20
   random_num = rand(100)

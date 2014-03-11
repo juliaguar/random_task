cc94c597744d66fd3e2dec69a5439bf6a120dd95
Finn <derfinn@gmail.com>
2014-03-11 22:40:00 +0100

    Distance calc

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,11 @@
 $(function() {
 	$('body').mousemove(function(e) {
-		$height = $('body').width();
-		$('.background-wrapper').css('-webkit-filter', 'blur(' + Math.floor(Math.abs(e.pageX - $height/2) / $height * 20) + 'px)');
-		console.log("move");
+		var width = $('body').width();
+		var height = $('body').height();
+		var distance_width = Math.abs(e.pageX - width/2) / width;
+		var distance_height = Math.abs(e.pageY - height/2) / height;
+		var distance = Math.sqrt(Math.pow(distance_height, 2) + Math.pow(distance_width, 2));
+		$('.background-wrapper').css('-webkit-filter', 'blur(' + Math.floor(distance * 20) + 'px)');
+		$('.background-wrapper').css('-webkit-transform', 'scale(' + (1 + distance * 0.75) + ')');
 	});
 });

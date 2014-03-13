26a0eb98b28e36d2296d9793f27f74410f2c529b
Finn <derfinn@gmail.com>
2014-03-13 16:58:10 +0100

    Add environment specific config, hopefully fixes #2

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require 'yaml'
 set :public_folder, File.dirname(__FILE__) + '/static'
 images = YAML::load_file('config/images.yml')
 tasks = YAML::load_file('config/tasks.yml')
+config = YAML::load_file('config/config.yml')[settings.environment]
 
 get '/' do
   image = RandomTasks::BackgroundImage::from_hash(images.sample)
@@ -16,6 +17,7 @@ end
 get '/task.?:format?' do |format|
   task = RandomTasks::Task::from_hash(tasks.sample)
   task.image = RandomTasks::BackgroundImage::from_hash(images.sample)
+  task.time = config['task_time']
 
   if format == 'json'
     content_type :json

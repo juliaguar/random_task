e03e62de93504fe9275188a5b4c74df4a85bdcb0
Finn <derfinn@gmail.com>
2014-03-26 21:14:01 +0100

    Added mongodb. Looks weird but works so far

--- app.rb
+++ app.rb
@@ -5,18 +5,20 @@ require 'json'
 require 'yaml'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
-images = YAML::load_file('config/images.yml')
-tasks = YAML::load_file('config/tasks.yml')
 config = YAML::load_file('config/config.yml')[settings.environment]
+storage = RandomTasks::Storage.new(config['db_uri'])
+RandomTasks::Collection::set_storage(storage)
+background_images = RandomTasks::Collection.new(RandomTasks::BackgroundImage, 'images')
+tasks = RandomTasks::Collection.new(RandomTasks::Task, 'tasks')
 
 get '/' do
-  image = RandomTasks::BackgroundImage::from_hash(images.sample)
+  image = background_images.get_random
   haml :index, :locals => {:task => nil, :image => image}
 end
 
 get '/task.?:format?' do |format|
-  task = RandomTasks::Task::from_hash(tasks.sample)
-  task.image = RandomTasks::BackgroundImage::from_hash(images.sample)
+  task = tasks.get_random
+  task.image = background_images.get_random
   task.time = config['task_time']
 
   if format == 'json'
--- lib/randomtasks.rb
+++ lib/randomtasks.rb
@@ -1,8 +1,58 @@
 require 'json'
+require 'mongo'
+require 'uri'
+require 'yaml'
 
 module RandomTasks
+	class Storage
+		attr_accessor :connection
+
+		def initialize(db_uri)
+			db = URI.parse(db_uri)
+			db_name = db.path.gsub(/^\//, '')
+			@connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+			@connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+		end
+
+		def get_collection(name)
+			@connection.collection(name)
+		end
+	end
+
+	class Collection
+
+		def initialize(class_type, collection_name)
+			@collection_name = collection_name
+			@class_type = class_type
+			@collection = @@storage.get_collection(@collection_name)
+		end
+
+		def self.set_storage(storage)
+			@@storage = storage
+		end
+
+		def get_all
+			records = []
+			@collection.find.each do |row|
+				records << @class_type::from_hash(row)
+			end
+			records
+		end
+
+		def get_random
+			total = @collection.count
+			skip = if total > 1 then Random.rand(total) else 0 end
+			raw = @collection.find_one({}, {
+				:skip =>  skip
+			})
+			@class_type::from_hash(raw)
+		end
+	end
+
 	class Task
 		attr_accessor :title, :time, :image
+
+		@@collection_name = 'tasks'
 
 		def initialize(title, time = 10)
 			@title = title
@@ -20,6 +70,8 @@ module RandomTasks
 
 	class BackgroundImage
 		attr_accessor :url, :credits
+
+		@@collection_name = 'images'
 
 		def initialize(url, credits)
 			@url = url
